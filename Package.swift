c0141be0e3535ef3c58da69600c7c0239d468632
Pete Richardson <peterichardson@apple.com>
2020-01-18 15:50:18 -0800

    using system library target instead

--- Package.swift
+++ Package.swift
@@ -4,4 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "Cncurses"
+
+    products: [
+        .library(name: "Cncurses", targets: ["Cncurses"]),
+    ],
+    targets: [
+        .systemLibrary(name: "Cncurses", path: "."),
+    ]
 )
